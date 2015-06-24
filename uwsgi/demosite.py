73b771ae92310ffce4d16abc51cdc1d14915026e
Shawn Aten <shawnaten@users.noreply.github.com>
2015-06-24 17:20:47 -0500

    Update demosite.py

--- uwsgi/demosite.py
+++ uwsgi/demosite.py
@@ -20,7 +20,7 @@ def application(env, start_response):
     with tag('body'):
         with tag('div', klass = 'container-fluid'):
             with tag('div', klass = 'row'):
-                with tag('div', klass = 'col-xs-12 col-md-4 col-md-offset-4'):
+                with tag('div', klass = 'col-xs-12 col-md-6 col-md-offset-3'):
                     with tag('h1', klass = 'text-center'):
                         text('Chameleon Cloud Docker Demo')
                     input_file = codecs.open("description.md", mode="r")
