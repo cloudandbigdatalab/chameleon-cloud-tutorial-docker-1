98b13f9b20e7eeced345b432782f59b3513ccc15
shawnaten <shawnmaten@gmail.com>
2015-06-25 12:07:52 -0500

    demosite sql

---
+++ postgres/demosite.sql
@@ -0,0 +1,19 @@
+create table demosite
+(
+  id serial primary key,
+  name text
+);
+
+insert into demosite (name) values
+(
+  Cirrus,
+  Cirrostratus,
+  Cirrocumulus,
+  Altostratus,
+  Altocumulus,
+  Nimbostratus,
+  Cumulus,
+  Stratus,
+  Cumulonimbus,
+  Stratocumulus
+);
